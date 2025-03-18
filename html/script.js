6e94bdfdf41b75ff6407c3e3bd9a65cbd6e3d993
sabrina0809 <souzalaurindo49@gmail.com>
2025-03-17 22:17:01 -0300

    mexendo com o java

---
+++ html/script.js
@@ -0,0 +1,20 @@
+/*console.log(2+2);
+console.log("quero ir embora");
+
+var primeiroTipo;
+let segundoTipo;
+const terceiroTipo = 10;
+
+let nome = "Kitty";
+let idade = 12;
+
+console.log(nome);
+console.log(idade);
+
+nome = idade;
+
+console.log(nome);*/
+function hello(){
+    document.getElementById('texto').textContent = "Hello Kitty";
+    document.getElementById('texto').style.color = "purple"
+}
